e303ba8c9338161065af41b6cec5032a440e5f86
CarterCasey <Carter.Casey@tufts.edu>
2015-02-08 18:14:04 -0500

    Solid draft of index and bio, css

--- tufts-pub/playground/findHidden.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for p in `find ~ -name "*.jpg" 2> /dev/null`; do
-	if [ -z `file "$p" | sed -e 's/.*jpg://' | grep -e JPEG` ]; then
-		file "$p"
-	fi
-done | grep -vi "No such file" 2> /dev/null > .non-jpgs
