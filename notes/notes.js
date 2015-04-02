c9776a0217b01d490b85c791b287efc819fd0ce7
CarterCasey <Carter.Casey@tufts.edu>
2015-04-02 00:31:21 -0400

    Some changes, added catnap reading notes

--- notes/notes.js
+++ notes/notes.js
@@ -1,6 +1,6 @@
 /* Code mostly used to move between pages of notes.
- * 
- *
+ * Does linking between class pages, which is really
+ * a job that I should be using php for... 
  */
 
 var CLASSES = {ids:"IDS", networks:"networks", compilers:"compilers", web:"web"};
@@ -8,7 +8,7 @@ var CLASSES = {ids:"IDS", networks:"networks", compilers:"compilers", web:"web"}
 $("document").ready(displayContent);
 
 // Put links in the body
-// of the page
+// of the page.
 function displayContent() {
 	var class_query = getClass();
 
@@ -24,8 +24,8 @@ function displayContent() {
 // links in the body
 function listClasses() {
 	for (q in CLASSES) {
-		$("#class-list").append("<p><a href=\"?class=" + q + "\">"
-								+ CLASSES[q] + "</a></p>");
+		$("#class-list").append("<div><a href=\"?class=" + q + "\">"
+								+ CLASSES[q] + "</a></div>");
 	}
 }
 
@@ -34,11 +34,13 @@ function getClass() {
 	var url = window.location.href;
 
 	// we only care about the match for class
-	var match = url.match(/\?.*class=(\w+)\&?.*/);
+	var match = url.match(/\?.*class=(\w+)(?:\&.*)?/);
 	if (match) {
 		return match[1];
 	} else {
 		return "";
 	}
 }
+
+
 
