7a82d0e6b6ca0c906e3573e5d2ad89e165f30aab
CarterCasey <Carter.Casey@tufts.edu>
2015-08-19 21:26:54 -0400

    The colors of bubbles should now be modifiable.

--- bubbles/script.js
+++ bubbles/script.js
@@ -1,12 +1,33 @@
-var MAX_SIZE = 80;
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+
+$(document).ready(function () {
+	$("#settings-button").click(function () { $("#settings").toggle(); });
+});
 
 $(window).load(function () {
-
 	var interval_id = null;
+	var max_size = 80;
+	var colors = {
+		red: {
+			min: 0,
+			max: 255,
+		},
+		green: {
+			min: 0,
+			max: 255,
+		},
+		blue: {
+			min: 0,
+			max: 255,
+		},	
+	};
+	
 	$("#background-ripple").on("iron-change", function () {
 		if (this.checked) {
-			backgroundBubbles();
-			interval_id = window.setInterval(backgroundBubbles, 2000);
+			backgroundBubbles(colors, max_size);
+			interval_id = window.setInterval(function () {
+				backgroundBubbles(colors, max_size);
+			}, 2000);
 		} else {
 			window.clearInterval(interval_id);
 		}
@@ -15,40 +36,65 @@ $(window).load(function () {
 	$("#cursor-ripple").on("iron-change", function () {
 		if (this.checked) {
 			$(document).mousemove(function (event) {
-				blowBubble(event.pageY, event.pageX,
-						   randInt(MAX_SIZE - 50, MAX_SIZE),
-						   "rgb(" + randInt(0, 255) + "," 
-								  + randInt(0, 255) + ","
-								  + randInt(0, 255) + ")"
-				);
+				mouseBubble(event, colors, max_size);
 			});
 		} else {
 			$(document).off("mousemove");
 		}
 	});
+	
+	$(".color-setting").on("value-change", function () {
+		colors[$(this).data("color")][$(this).data("target")] = this.value;
+	})
 
 });
 
-function backgroundBubbles () {
-	$(".ink").remove();
+function mouseBubble (event, colors, max_size) {
+	var top   = event.pageY - 20;
+	var left  = event.pageX - 20;
+	var size  = randInt(max_size - 50, max_size);
+	var color = "rgb(" + randInt(colors.red.min, colors.red.max) +  "," 
+					+ randInt(colors.green.min, colors.green.max) + ","
+					+ randInt(colors.blue.min, colors.blue.max) +   ")";
+	
+	blowBubble(top, left, size, color);
+}
 
+function backgroundBubbles (colors, max_size) {
 	var window_width = $(window).width();
 	var window_height = $(window).height();
 
 	for (var i = 0; i < 100; i++) {
 		window.setTimeout(function () {
-			blowBubble(randInt(0, window_height),
-					   randInt(0, window_width),
-					   randInt(MAX_SIZE - 50, MAX_SIZE),
-					   "rgb(" + randInt(0, 255) + "," 
-							  + randInt(0, 255) + ","
-							  + randInt(0, 255) + ")"
-			);
+			var top   = randInt(0, window_height);
+			var left  = randInt(0, window_width);
+			var size  = randInt(max_size - 50, max_size);
+			var color = "rgb(" + randInt(colors.red.min, colors.red.max) +     "," 
+							   + randInt(colors.green.min, colors.green.max) + ","
+							   + randInt(colors.blue.min, colors.blue.max) +   ")";
+			
+			blowBubble(top, left, size, color);
 		}, i * 10);
 	};
 }
 
 function randInt (min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function blowBubble (top, left, size, color) {
+	var bubble = $("<div class='bubble'></div>");
+
+	bubble.css({
+		"top": top, "left": left,
+		"background-color": color,
+		"width": size, "height": size,
+	});
+
+	$("body").append(bubble);
+
+	bubble.addClass("animate");
+
+	setTimeout(function () { bubble.remove() }, 3000)
 }
 
