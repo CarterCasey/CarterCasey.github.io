9e8b3982b65931c57ee98458a27c89f21ba778c8
CarterCasey <Carter.Casey@tufts.edu>
2015-08-21 17:56:00 -0400

    Factored control panel into polymer element, added controls for min colors and size of bubbles.

--- bubbles/script.js
+++ bubbles/script.js
@@ -1,32 +1,35 @@
 /// <reference path="../typings/jquery/jquery.d.ts"/>
 
 $(document).ready(function () {
-	$("#settings-button").click(function () { $("#settings").toggle(); });
+	$("#control-button").click(function () { $("#control-panel").toggle(); });
 });
 
 $(window).load(function () {
 	var interval_id = null;
-	var max_size = 80;
+	var size_range = {
+		min: +$(".size-setting[data-target='min']").attr("value"),
+		max: +$(".size-setting[data-target='max']").attr("value"),
+	};
 	var colors = {
 		red: {
-			min: +$(".color-setting.red").attr("min"),
-			max: +$(".color-setting.red").attr("max"),
+			min: +$(".color-setting.red[data-target='min']").attr("value"),
+			max: +$(".color-setting.red[data-target='max']").attr("value"),
 		},
 		green: {
-			min: +$(".color-setting.green").attr("min"),
-			max: +$(".color-setting.green").attr("max"),
+			min: +$(".color-setting.green[data-target='min']").attr("value"),
+			max: +$(".color-setting.green[data-target='max']").attr("value"),
 		},
 		blue: {
-			min: +$(".color-setting.blue").attr("min"),
-			max: +$(".color-setting.blue").attr("max"),
-		},	
+			min: +$(".color-setting.blue[data-target='min']").attr("value"),
+			max: +$(".color-setting.blue[data-target='max']").attr("value"),
+		},
 	};
 	
 	$("#background-ripple").on("iron-change", function () {
 		if (this.checked) {
-			backgroundBubbles(colors, max_size);
+			backgroundBubbles(colors, size_range);
 			interval_id = window.setInterval(function () {
-				backgroundBubbles(colors, max_size);
+				backgroundBubbles(colors, size_range);
 			}, 2000);
 		} else {
 			window.clearInterval(interval_id);
@@ -36,7 +39,7 @@ $(window).load(function () {
 	$("#cursor-ripple").on("iron-change", function () {
 		if (this.checked) {
 			$(document).mousemove(function (event) {
-				mouseBubble(event, colors, max_size);
+				mouseBubble(event, colors, size_range);
 			});
 		} else {
 			$(document).off("mousemove");
@@ -44,15 +47,41 @@ $(window).load(function () {
 	});
 	
 	$(".color-setting").on("value-change", function () {
-		colors[$(this).data("color")][$(this).data("target")] = this.value;
+		var target = $(this).data("target");
+		var color  = $(this).data("color");
+		var value  = this.value;
+		
+		var opposite = (target == "max") ? "min" : "max";
+		var other_setting = $(".color-setting." + color + "[data-target='" + opposite + "']")[0]; 
+		var other_value = other_setting.value;
+		
+		if ((target == "max") != (value >= other_value)) {
+			other_setting.value = value;
+			colors[color][opposite] = value;			
+		}
+		colors[color][target] = value;
+	});
+	
+	$(".size-setting").on("value-change", function () {
+		var target = $(this).data("target");
+		var value  = this.value;
+		
+		var opposite = (target == "max") ? "min" : "max";
+		var other_setting = $(".size-setting[data-target='" + opposite + "']")[0];
+		var other_value = other_setting.value;
+		
+		if ((target == "max") != (value >= other_value)) {
+			other_setting.value = value;
+			size_range[opposite] = other_setting.value;
+		}
+		size_range[target] = value;
 	})
-
 });
 
-function mouseBubble (event, colors, max_size) {
+function mouseBubble (event, colors, size_range) {
 	var top   = event.pageY - 20;
 	var left  = event.pageX - 20;
-	var size  = randInt(max_size - 50, max_size);
+	var size  = randInt(size_range.min, size_range.max);
 	var color = "rgb(" + randInt(colors.red.min, colors.red.max) +     "," 
 					   + randInt(colors.green.min, colors.green.max) + ","
 					   + randInt(colors.blue.min, colors.blue.max) +   ")";
@@ -60,7 +89,7 @@ function mouseBubble (event, colors, max_size) {
 	blowBubble(top, left, size, color);
 }
 
-function backgroundBubbles (colors, max_size) {
+function backgroundBubbles (colors, size_range) {
 	var window_width = $(window).width();
 	var window_height = $(window).height();
 
@@ -68,7 +97,7 @@ function backgroundBubbles (colors, max_size) {
 		window.setTimeout(function () {
 			var top   = randInt(0, window_height);
 			var left  = randInt(0, window_width);
-			var size  = randInt(max_size - 50, max_size);
+			var size  = randInt(size_range.min, size_range.max);
 			var color = "rgb(" + randInt(colors.red.min, colors.red.max) +     "," 
 							   + randInt(colors.green.min, colors.green.max) + ","
 							   + randInt(colors.blue.min, colors.blue.max) +   ")";
