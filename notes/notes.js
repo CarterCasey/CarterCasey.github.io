fa69891d06ee362857b8141e6449ff08418bbf80
CarterCasey <Carter.Casey@tufts.edu>
2015-11-24 21:47:34 -0500

    Added some rudimentary navigation to my notes

--- notes/notes.js
+++ notes/notes.js
@@ -1,11 +1,10 @@
-/* Code mostly used to move between pages of notes.
- * Does linking between class pages, which is really
- * a job that I should be using php for... 
- */
+/* Code mostly used to move between pages of notes. */
 
 var CLASSES = {ids:"IDS", networks:"networks", compilers:"compilers", web:"web"};
 
 $("document").ready(displayContent);
+
+$(document).keydown(navigation);
 
 // Put links in the body
 // of the page.
@@ -13,8 +12,8 @@ function displayContent() {
 	var class_query = getClass();
 
 	if (class_query in CLASSES) {
-		window.location.replace("https://cartercasey.github.io/notes/" +
-								CLASSES[class_query] + "/1.html");
+		var new_path = "/notes/" + CLASSES[class_query] + "/1.html";
+		window.location.replace(new_path);
 	} else {
 		listClasses();
 	}
@@ -23,7 +22,7 @@ function displayContent() {
 // Just give the top-menu
 // links in the body
 function listClasses() {
-	for (q in CLASSES) {
+	for (var q in CLASSES) {
 		$("#class-list").append("<div><a href=\"?class=" + q + "\">"
 								+ CLASSES[q] + "</a></div>");
 	}
@@ -42,5 +41,19 @@ function getClass() {
 	}
 }
 
-
+function navigation(event) {
+	if (event.which == 37 || event.which == 39) {
+		var url = window.location.href;
+		var match = url.match(/.*\/(\d+)\.html/);
+		var page;
+		if (match) { page = parseInt(match[1]) } else { return; }
+		
+		var new_page = (event.which == 37) ? page - 1 : page + 1;
+		
+		var new_url = url.replace(/\d+\.html/, new_page + ".html");
+		window.location.replace(new_url);
+	} else {
+		return;
+	}
+}
 
